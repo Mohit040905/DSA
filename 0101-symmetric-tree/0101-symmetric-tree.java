c2d233cc1cdfbc29f06cfb49adc0aae68a79fc57
Mohit Morande <mohitmorande098@gmail.com>
2026-07-13 23:25:13 +0530

    Time: 0 ms (100%), Space: 43.3 MB (85.68%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -16,7 +16,7 @@
 class Solution {
     public boolean symm(TreeNode Node1, TreeNode Node2){
         if(Node1 == null && Node2 == null) return true;
-        if((Node1!=null && Node2 == null )||(Node1 == null && Node2 != null)) return false;
+        if(Node1 == null || Node2 == null) return false;
         if(Node1.val != Node2.val) return false;
         return symm(Node1.left, Node2.right) && symm(Node2.left, Node1.right);
     }
