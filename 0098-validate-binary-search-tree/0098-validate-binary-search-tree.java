6985d9cb9c2bc5a92376e3aa12eb673579233e7e
Mohit Morande <mohitmorande098@gmail.com>
2026-09-13 16:36:14 +0530

    Time: 1 ms (16.42%), Space: 45.1 MB (50.85%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,15 +14,32 @@
  * }
  */
 class Solution {
-    public TreeNode temp = null;
-    public boolean inorder(TreeNode root){
-        if(root==null) return true;
-        if(!inorder(root.left)) return false;
-        if(temp!=null && root.val <= temp.val ) return false;
-        temp = root;
-        return inorder(root.right);
-    }
     public boolean isValidBST(TreeNode root) {
-        return inorder(root);
+        TreeNode curr = root;
+        long prev = Long.MIN_VALUE;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode pred = curr.left;
+                while(pred.right !=null && pred.right!= curr){
+                    pred = pred.right;
+                }
+                if(pred.right==null){
+                    pred.right = curr;
+                    curr = curr.left;
+                }
+                else{
+                    pred.right = null;
+                    if(curr.val<=prev) return false;
+                prev = curr.val;
+                    curr = curr.right;
+                }
+            }
+            else{
+                if(curr.val<=prev) return false;
+                prev = curr.val;
+                curr = curr.right;
+            }
+        }
+        return true;
     }
 }
